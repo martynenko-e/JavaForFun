114c350e3448021233aa071005e9c599fe99faac
Martynenko <martynenko.evhen@gmail.com>
2016-01-21 20:14:23 +0200

    add bootstrap css and js

--- VacancyParser/src/main/java/Aggregator.java
+++ VacancyParser/src/main/java/Aggregator.java
@@ -2,6 +2,7 @@ import dao.VacancyDao;
 import model.CiklumStrategy;
 import model.HHStrategy;
 import model.Provider;
+import valueobject.Vacancy;
 
 /**
  * Created by Martynenko on 14.01.2016.
@@ -12,6 +13,7 @@ public class Aggregator {
         Provider provider = new Provider(new HHStrategy());
         Provider provider2 = new Provider(new CiklumStrategy());
         Controller controller = new Controller(provider, provider2);
+        VacancyDao.getInstance().createTable();
         controller.scan();
         controller.addVacanciesToDb();
 
